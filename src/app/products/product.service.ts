128ffbcab63fa9b4d50519234c0898e568d56ddf
ArgyleChen <phil.cc.chen@gmail.com>
2023-04-26 21:36:39 +1200

    getProduct() service added

---
+++ src/app/products/product.service.ts
@@ -0,0 +1,46 @@
+//2. import the necessary interface and components. 
+import { Injectable } from "@angular/core";
+import { IProduct } from "./product";
+
+//3. Add directive. 
+@Injectable({
+    providedIn: "root"
+})
+
+//1. create the service's class and method. 
+export class ProductService {
+getProduct() : IProduct[] {
+    return [
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2021",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "assets/images/garden_cart.png"
+          },
+          {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2021",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "assets/images/hammer.png"
+          },
+          {
+            "productId": 10,
+            "productName": "Video Game Controller",
+            "productCode": "GMG-0042",
+            "releaseDate": "October 15, 2020",
+            "description": "Standard two-button video game controller",
+            "price": 35.95,
+            "starRating": 4.6,
+            "imageUrl": "assets/images/xbox-controller.png"
+          }
+    ]
+}
+}
